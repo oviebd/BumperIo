ce0709f3ac1d658723ff6357c67e18625cdd6040
oviebd <oviecse48@gmail.com>
2021-03-20 23:14:55 +0600

    Move with Rigidbody (Bug - Delay with keyboard input)

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-   
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float _moveSpeed = 5.0f;
+	[SerializeField] private float _rotationSpeed = 500.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector3 _moveDirection;
+	private Vector3 _previousDirection = Vector3.zero;
+
+	Vector2 inputData;
+	float _horizontalValue, _verticalValue;
+
+	void Update()
+	{
+		_horizontalValue = Input.GetAxis("Horizontal");
+		_verticalValue = Input.GetAxis("Vertical");
+		// Debug.Log("Vertical Value " + _verticalValue);
+		inputData = new Vector2(_horizontalValue, _verticalValue);
+		_moveDirection = new Vector3(_horizontalValue, 0, _verticalValue);
+		_moveDirection.Normalize();
+
+
+		if (_moveDirection != Vector3.zero)
+		{
+			Debug.Log("Move Direction - " + _moveDirection);
+			transform.Translate(_moveDirection * _moveSpeed * Time.deltaTime, Space.World);
+			Quaternion rotation = Quaternion.LookRotation(_moveDirection, Vector3.up);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
+			_previousDirection = _moveDirection;
+		}
+		else
+		{
+			transform.Translate(_previousDirection * _moveSpeed * Time.deltaTime, Space.World);
+		}
+
+	}
+
+	private void FixedUpdate()
+	{
+	}
+
+	private void MoveRorward()
+	{
+
+	}
+
+	private void MoveBackWard()
+	{
+
+	}
+
+	private void MoveRight()
+	{
+
+	}
+
+	private void MoveLeft()
+	{
+
+	}
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,8 +7,12 @@ public class PlayerMove : MonoBehaviour
 	[SerializeField] private float _moveSpeed = 5.0f;
 	[SerializeField] private float _rotationSpeed = 5.0f;
 
+	[SerializeField] private Rigidbody _rb;
+
 	private Vector3 _moveDirection;
 	private Vector3 _previousDirection = Vector3.zero;
+
+	Quaternion _prevRotation = Quaternion.LookRotation(Vector3.zero, Vector3.up);
 
 	Vector2 inputData;
 	float _horizontalValue, _verticalValue;
@@ -17,27 +21,31 @@ public class PlayerMove : MonoBehaviour
 	{
 		_horizontalValue = Input.GetAxis("Horizontal");
 		_verticalValue = Input.GetAxis("Vertical");
-		// Debug.Log("Vertical Value " + _verticalValue);
-		inputData = new Vector2(_horizontalValue, _verticalValue);
-		_moveDirection = new Vector3(_horizontalValue, 0, _verticalValue);
-		_moveDirection.Normalize();
-
-			if(_moveDirection != Vector3.zero)
-			{
-			transform.Translate(_moveDirection * _moveSpeed * Time.deltaTime, Space.World);
-			Quaternion rotation = Quaternion.LookRotation(_moveDirection, Vector3.up);
-			transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
-			_previousDirection = _moveDirection;
-		}
-		else
-		{
-			transform.Translate( _previousDirection * _moveSpeed* Time.deltaTime, Space.World);
-		}
-
+	     Debug.Log("Vertical Value " + _verticalValue + "  hor val "  + _horizontalValue);
+	   
 	}
 
 	private void FixedUpdate()
 	{
+		inputData = new Vector2(_horizontalValue, _verticalValue);
+		_moveDirection = new Vector3(_horizontalValue, 0, _verticalValue);
+		_moveDirection.Normalize();
+
+		if(_moveDirection != Vector3.zero)
+		{
+			//Debug.Log("Move Direction - " + _moveDirection);
+			_rb.MovePosition(transform.position + _moveDirection * _moveSpeed * Time.fixedDeltaTime);
+			Quaternion rotation = Quaternion.LookRotation(_moveDirection, Vector3.up);
+			_rb.MoveRotation(rotation);
+			_previousDirection = _moveDirection;
+			_prevRotation = rotation;
+		}
+		else
+		{
+			//Debug.Log("Move Direction "  +  _moveDirection +"Prev Direction - " + _previousDirection);
+			_rb.MovePosition(transform.position + _previousDirection * _moveSpeed * Time.fixedDeltaTime);
+			_rb.MoveRotation(_prevRotation);
+		}
 	}
 
 	private void MoveRorward()
@@ -59,9 +67,5 @@ public class PlayerMove : MonoBehaviour
 	{
 
 	}
-
-	
-
-	
 
 }
